6a6cde68e1886ced4df0f04ff83516775f802599
JulienCodron <78977430+JulienCodron@users.noreply.github.com>
2022-03-10 19:34:11 +0100

    start AI
    Add AI for bot, only setup BP for this

--- Source/Bomberman/BombermanCharacter.cpp
+++ Source/Bomberman/BombermanCharacter.cpp
@@ -24,38 +24,7 @@ ABombermanCharacter::ABombermanCharacter()
 
 	NbCellExplosed = 2;
 
-
-	// Don't rotate when the controller rotates. Let that just affect the camera.
-	//bUseControllerRotationPitch = false;
-	//bUseControllerRotationYaw = false;
-	//bUseControllerRotationRoll = false;
-
-	// Configure character movement
 	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
-	//GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
-	//GetCharacterMovement()->JumpZVelocity = 600.f;
-	//GetCharacterMovement()->AirControl = 0.2f;
-
-	/*
-	// Create a camera boom (pulls in towards the player if there is a collision)
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 500.0f; // The camera follows at this distance behind the character	
-	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
-	CameraBoom->SetRelativeRotation(FRotator(-70.f, 0.f, 0.f));
-
-	CameraBoom->bInheritPitch = false;
-	CameraBoom->bInheritRoll = false;
-	CameraBoom->bInheritYaw = false;
-
-	// Create a follow camera
-	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
-	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
-
-	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
-	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
-	*/
 }
 
 //////////////////////////////////////////////////////////////////////////
--- Source/Bomberman/BombermanCharacter.h
+++ Source/Bomberman/BombermanCharacter.h
@@ -11,13 +11,6 @@ class ABombermanCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
-	/** Camera boom positioning the camera behind the character */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* CameraBoom;
-
-	/** Follow camera */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* FollowCamera;
 public:
 	ABombermanCharacter();
 
@@ -51,11 +44,6 @@ protected:
 	// End of APawn interface
 
 public:
-	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	/** Returns FollowCamera subobject **/
-	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
-
 	void AddBomb();
 	void RemoveBomb();
 
